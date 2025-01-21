0c3f6fc3b8dbeb8377e94e66d0ee6cd16f5a7c9a
Richard Samuels <richard.l.samuels@gmail.com>
2025-01-21 18:53:11 -0500

    refactor redirector to two separate runners

--- src/redirector.rs
+++ src/redirector.rs
@@ -12,113 +12,78 @@ use tracing::{error, trace, trace_span};
 // See tests/mtu.rs for an explanation of this magic number
 pub const PROTOCOL_OVERHEAD: u16 = 53;
 
-/// Reads data from stream, and send it along the `tx` channel
-/// Reads data from rx chnnale, and send it along the stream
-pub struct Redirector<R, W> {
+pub fn with_stream<R, W>(
     id: SocketAddr,
     port: u16,
-    buffer_size: u16,
+    mtu: u16,
     token: CancellationToken,
-    reader: BufReader<R>,
-    writer: BufWriter<W>,
-    tx: mpsc::Sender<stnet::RedirectorFrame>,
-    rx: mpsc::Receiver<stnet::RedirectorFrame>,
-}
 
-impl Redirector<OwnedReadHalf, OwnedWriteHalf> {
-    pub fn with_stream(
-        id: SocketAddr,
-        port: u16,
-        mtu: u16,
-        token: CancellationToken,
-        stream: tnet::TcpStream,
-        tx: mpsc::Sender<stnet::RedirectorFrame>,
-        rx: mpsc::Receiver<stnet::RedirectorFrame>,
-    ) -> Self {
-        // Look, what we're really trying to do here is calculate
-        // MTU - PROTOCOL_OVERHEAD - (TCP/UDP Overhead) - Quic Overhead - TLS overhead
-        // But i'm way too lazy to plumb all the required info, so
-        // Let's just use 1330, since ipv6 + QUIC says 1330 per packet
-        // Citation: https://blog.apnic.net/2019/03/04/a-quick-look-at-quic/
-        //let buffer_size = mtu - PROTOCOL_OVERHEAD;
-        let buffer_size = 1330;
-
-        let (reader, writer) = stream.into_split();
-        let reader = BufReader::with_capacity(buffer_size as usize, reader);
-        let writer = BufWriter::with_capacity(buffer_size as usize, writer);
-
-        Redirector {
-            id,
-            port,
-            buffer_size,
-            token,
-            tx,
-            rx,
-            reader,
-            writer,
-        }
-    }
-}
-
-impl<R, W> Redirector<R, W>
+    chan2net: mpsc::Receiver<stnet::RedirectorFrame>,
+    net2chan: mpsc::Sender<stnet::RedirectorFrame>,
+    stream: tnet::TcpStream,
+) -> (
+    Redirect2Channel<OwnedReadHalf>,
+    Redirect2Network<OwnedWriteHalf>,
+)
 where
     R: AsyncRead + std::marker::Unpin,
     W: AsyncWrite + std::marker::Unpin,
 {
-    pub fn new(
+    let (reader, writer) = stream.into_split();
+
+    let r = Redirect2Channel::with_reader(id, port, mtu, token.clone(), reader, net2chan);
+    let w = Redirect2Network::with_writer(id, mtu, token.clone(), writer, chan2net);
+    (r, w)
+}
+
+pub struct Redirect2Network<W> {
+    id: SocketAddr,
+    #[allow(dead_code)]
+    buffer_size: u16,
+    token: CancellationToken,
+    writer: BufWriter<W>,
+    rx: mpsc::Receiver<stnet::RedirectorFrame>,
+}
+
+impl<W> Redirect2Network<W>
+where
+    W: AsyncWrite + std::marker::Unpin,
+{
+    pub fn with_writer(
         id: SocketAddr,
-        port: u16,
-        buffer_size: u16,
+        _mtu: u16,
         token: CancellationToken,
-        reader: BufReader<R>,
-        writer: BufWriter<W>,
-        tx: mpsc::Sender<stnet::RedirectorFrame>,
+        writer: W,
         rx: mpsc::Receiver<stnet::RedirectorFrame>,
-    ) -> Redirector<R, W> {
-        Redirector {
+    ) -> Self {
+        // Look, what we're really trying to do here is calculate
+        // MTU - PROTOCOL_OVERHEAD - (TCP/UDP Overhead) - Quic Overhead - TLS overhead
+        // But i'm way too lazy to query these values in a cross platofmr way,
+        // plumb the required info here, and figure out the math so....
+        // Let's just use 1330, since ipv6 + QUIC says 1330 per packet
+        // Citation: https://blog.apnic.net/2019/03/04/a-quick-look-at-quic/
+        //let buffer_size = mtu - PROTOCOL_OVERHEAD;
+        let buffer_size = 1330;
+        let writer = BufWriter::with_capacity(buffer_size as usize, writer);
+
+        Redirect2Network {
             id,
-            port,
-            token,
-            reader,
-            writer,
-            tx,
-            rx,
             buffer_size,
+            token,
+            rx,
+            writer,
         }
     }
+
     pub async fn run(&mut self) {
-        let span = trace_span!("tunnel start", addr = ?self.id);
+        let span = trace_span!("channel2network", addr = ?self.id);
         let _guard = span.enter();
 
-        let mut buf = vec![0u8; self.buffer_size as usize];
         let mut last_activity = std::time::Instant::now();
         let keepalive = Duration::from_secs(300);
         let mut interval = tokio::time::interval(keepalive);
         loop {
             tokio::select! {
-                maybe_len = self.reader.read(&mut buf) => {
-                    let len = match maybe_len {
-                        Err(e) => {
-                            error!(addr = ?self.id, cause = ?e, "failed to read from network");
-                            break;
-                        },
-                        Ok(l) => l,
-                    };
-                    if len == 0 {
-                        let _ = self.tx.send(stnet::RedirectorFrame::KillListener(self.id)).await;
-                        trace!("read 0 bytes, ending redirector");
-                        break
-                    }
-                    let d = stnet::Datagram {
-                        id: self.id,
-                        port: self.port,
-                        data: buf[0..len].to_vec(),
-                    };
-                    self.reader.consume(len);
-                    let _ = self.tx.send(d.into()).await;
-                    last_activity = Instant::now();
-                }
-
                 maybe_data = self.rx.recv() => {
                     let data = match maybe_data {
                         None => break,
@@ -151,6 +116,113 @@ where
             }
         }
         self.rx.close();
-        trace!("Tunnel end");
+        trace!("channel2network end");
+    }
+}
+
+/// Reads data from stream, and send it along the `tx` channel
+/// Reads data from rx chnnale, and send it along the stream
+pub struct Redirect2Channel<R> {
+    id: SocketAddr,
+    port: u16,
+    buffer_size: u16,
+    token: CancellationToken,
+    reader: BufReader<R>,
+    tx: mpsc::Sender<stnet::RedirectorFrame>,
+}
+
+impl<R> Redirect2Channel<R>
+where
+    R: AsyncRead + std::marker::Unpin,
+{
+    pub fn with_reader(
+        id: SocketAddr,
+        port: u16,
+        _mtu: u16,
+        token: CancellationToken,
+        reader: R,
+        tx: mpsc::Sender<stnet::RedirectorFrame>,
+    ) -> Self {
+        // Look, what we're really trying to do here is calculate
+        // MTU - PROTOCOL_OVERHEAD - (TCP/UDP Overhead) - Quic Overhead - TLS overhead
+        // But i'm way too lazy to plumb all the required info, so
+        // Let's just use 1330, since ipv6 + QUIC says 1330 per packet
+        // Citation: https://blog.apnic.net/2019/03/04/a-quick-look-at-quic/
+        //let buffer_size = mtu - PROTOCOL_OVERHEAD;
+        let buffer_size = 1330;
+
+        let reader = BufReader::with_capacity(buffer_size as usize, reader);
+
+        Redirect2Channel {
+            id,
+            port,
+            buffer_size,
+            token,
+            tx,
+            reader,
+        }
+    }
+
+    pub fn new(
+        id: SocketAddr,
+        port: u16,
+        buffer_size: u16,
+        token: CancellationToken,
+        reader: BufReader<R>,
+        tx: mpsc::Sender<stnet::RedirectorFrame>,
+    ) -> Redirect2Channel<R> {
+        Redirect2Channel {
+            id,
+            port,
+            token,
+            reader,
+            tx,
+            buffer_size,
+        }
+    }
+    pub async fn run(&mut self) {
+        let span = trace_span!("network2channel", addr = ?self.id);
+        let _guard = span.enter();
+
+        let mut buf = vec![0u8; self.buffer_size as usize];
+        let mut last_activity = std::time::Instant::now();
+        let keepalive = Duration::from_secs(300);
+        let mut interval = tokio::time::interval(keepalive);
+        loop {
+            tokio::select! {
+                maybe_len = self.reader.read(&mut buf) => {
+                    let len = match maybe_len {
+                        Err(e) => {
+                            error!(addr = ?self.id, cause = ?e, "failed to read from network");
+                            break;
+                        },
+                        Ok(l) => l,
+                    };
+                    if len == 0 {
+                        let _ = self.tx.send(stnet::RedirectorFrame::KillListener(self.id)).await;
+                        trace!("read 0 bytes, ending redirector");
+                        break
+                    }
+                    let d = stnet::Datagram {
+                        id: self.id,
+                        port: self.port,
+                        data: buf[0..len].to_vec(),
+                    };
+                    self.reader.consume(len);
+                    let _ = self.tx.send(d.into()).await;
+                    last_activity = Instant::now();
+                }
+
+                _ = interval.tick() => {
+                    if last_activity.elapsed() >= keepalive {
+                        trace!("{} seconds passed without any activity. Closing.", keepalive.as_secs());
+                        break
+                    }
+                }
+
+                _ = self.token.cancelled() => break,
+            }
+        }
+        trace!("network2channel end");
     }
 }
